10943dfb70d9304293cd6ba7e7479752fadeb474
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-11-29 18:20:06 -0500

    Creating  a model comment, creatinh index.js
    is, content, date , using modile .export,
    index js. is going to store a post user and comment

---
+++ models/comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = Comment;
