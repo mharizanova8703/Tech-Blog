5ac6759a365ed60ceba21b92c97f784cee5e47ad
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-11-29 17:34:11 -0500

    updating mode folder user file
    user.init id, name, email, password
    Requering a config/connection

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+module.exports = User;
+
